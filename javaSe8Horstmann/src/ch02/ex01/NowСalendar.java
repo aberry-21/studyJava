eab7ec8e4dccba2c98bc7c27f583ebd523f20c95
aberry-21 <lot.lebedevok@yandex.ru>
2021-03-15 21:43:25 +0300

    add ch02

---
+++ javaSe8Horstmann/src/ch02/ex01/NowСalendar.java
@@ -0,0 +1,96 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Сalendar.java                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aberry <aberry@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/15 17:59:20 by aberry            #+#    #+#             */
+/*   Updated: 2021/03/15 17:59:20 by aberry           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/**
+ * Измените представленную в этой главе программу вывода календаря таким об­разом,
+ * чтобы неделя начиналась с воскресенья. Кроме того, организуйте пере­вод
+ * на новую строку в конце предыдущей, но только один раз.
+ *
+ * Original version:
+ *
+ * Mon Tue Wed Thu Fri Sat Sun
+ * 1   2   3   4   5   6   7
+ * 8   9   10  11  12  13  14
+ * 15  16  17  18  19  20  21
+ * 22  23  24  25  26  27  28
+ * 29  30
+ *
+ * My version:
+ *
+ * Sun Mon Tue Wed Thu Fri Sat
+ *     1   2   3   4   5   6
+ * 7   8   9   10  11  12  13
+ * 14  15  16  17  18  19  20
+ * 21  22  23  24  25  26  27
+ * 28  29  30
+ *
+ */
+
+package ch02.ex01;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.Random;
+import java.util.Scanner;
+
+public class NowСalendar {
+
+    private static int[] createMonthArray(LocalDate date){
+        int[] arrayMonth = new int[42];
+        int index = date.getDayOfWeek().getValue();
+        for (int i = 1; i < 32; i++) {
+            arrayMonth[index] = i;
+            index++;
+        }
+        return (arrayMonth);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(
+                "╔══╗╔══╗╔╗──╔═══╗╔╗─╔╗╔══╗─╔══╗╔═══╗\n"+
+                "║╔═╝║╔╗║║║──║╔══╝║╚═╝║║╔╗╚╗║╔╗║║╔═╗║\n"+
+                "║║──║╚╝║║║──║╚══╗║╔╗─║║║╚╗║║╚╝║║╚═╝║\n"+
+                "║║──║╔╗║║║──║╔══╝║║╚╗║║║─║║║╔╗║║╔╗╔╝\n"+
+                "║╚═╗║║║║║╚═╗║╚══╗║║─║║║╚═╝║║║║║║║║║\n"+
+                "╚══╝╚╝╚╝╚══╝╚═══╝╚╝─╚╝╚═══╝╚╝╚╝╚╝╚╝");
+        for (int month = 1; month < 13; month++) {
+            System.out.println("\n\n\t\t" + Month.of(month).toString() + '\n');
+            System.out.print(DayOfWeek.of(7).toString().substring(0, 3) + ' ');
+            for (int i = 1; i < 7; i++) {
+                System.out.print(DayOfWeek.of(i).toString().substring(0, 3) + ' ');
+            }
+            System.out.println();
+            LocalDate date = LocalDate.of(2021, month, 1);
+            int[] arrayMonth = createMonthArray(date);
+            int IndexEnd = date.getDayOfWeek().getValue() + 31;
+            for (int i = 0; i < IndexEnd; i++) {
+                if (arrayMonth[i] == 0){
+                    System.out.print(" " + '\t');
+                } else {
+                    System.out.print(Integer.toString(arrayMonth[i]) + '\t');
+                }
+                if (i > 0 && (i + 1) % 7 == 0) {
+                    System.out.println();
+                }
+            }
+        }
+        // create random object
+        Random randomno = new Random();
+
+        // check next int value
+        System.out.println("Next int value: " + randomno.);
+        System.out.println("Next int value: " + randomno.nextInt(10000));
+    }
+}
+
+
---
+++ javaSe8Horstmann/src/ch02/ex02/Answer.md
@@ -0,0 +1,64 @@
+Проанализируйте метод nextlnt () из класса Scanner. Является ли он методом доступа или модифицирующим методом и почему?
+А что можно сказать о методе nextlnt () из класса Random?
+
+"
+Если метод изменяет объект, для которого он вызывается, то такой метод называется модифицирующим.
+А если вызываемый метод оставляет объект без изменения, то он называется методом доступа.
+"
+
+1) Собственно, из javadoc:
+   
+   public int nextInt (int radix)
+   
+   Сканирует следующий токен ввода как int. Этот метод будет генерировать InputMismatchException, если следующий токен
+   не может быть переведен в допустимое значение int. Если перевод выполнен успешно,сканер продвигается мимо ввода.
+   
+   Соответственно, из определения следует, что NextInt() является модифицирующим методом.
+
+    public int nextInt(int radix) {
+        // Check cached result
+        if ((typeCache != null) && (typeCache instanceof Integer)
+            && this.radix == radix) {
+            int val = ((Integer)typeCache).intValue();
+            useTypeCache();
+            return val;
+        }
+        setRadix(radix);
+        clearCaches();
+        // Search for next int
+        try {
+            String s = next(integerPattern());
+            if (matcher.group(SIMPLE_GROUP_INDEX) == null)
+                s = processIntegerToken(s);
+            return Integer.parseInt(s, radix);
+        } catch (NumberFormatException nfe) {
+            position = matcher.start(); // don't skip bad token <-----------------------------------
+            throw new InputMismatchException(nfe.getMessage());
+        }
+    }
+
+
+2) Из определения следует, что метод nextlnt () из класса Random является методом доступа
+   """
+       public int nextInt(int bound) {
+           if (bound <= 0)
+               throw new IllegalArgumentException(BadBound);
+    
+           int r = next(31);
+           int m = bound - 1;
+           if ((bound & m) == 0)  // i.e., bound is a power of 2
+               r = (int)((bound * (long)r) >> 31);
+           else {
+               for (int u = r;
+                    u - (r = u % bound) + m < 0;
+                    u = next(31))
+                   ;
+           }
+           return r;
+       }
+   """
+   Объект остается без изменений 
+   
+
+   
+   
